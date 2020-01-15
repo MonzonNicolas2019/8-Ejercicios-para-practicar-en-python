efe53402c4545b90ab80cbff8e20685dfc3cef72
Nicolás A. Monzón <53285434+MonzonNicolas2019@users.noreply.github.com>
2020-01-15 11:27:27 -0300

    Espaciados
    Esto aplica a todo el proyecto pero solo lo remarco en este archivo. Entre los operadores en lo posible deja espacios, queda mas prolijo. El ejercicio esta bien hecho :)

--- #Ejercicio 3.py
+++ #Ejercicio 3.py
@@ -1,28 +1,23 @@
 #De verdad P para deter año bisiesto.
 
-anio=int(input("Introduzca el año:"))
+anio = int(input("Introduzca el año:"))
 
 
-contador=0		
+contador = 0		
 
-if anio%4==0:
-	contador=contador+1
+if anio % 4 == 0:
+	contador++
 	
 
-if anio%100==0:
-	contador=contador+1
+if anio % 100 == 0:
+	contador++
 	
 
-if anio%400==0:
-	contador=contador+1
+if anio % 400 == 0:
+	contador++
 	
 
-if contador==0 or contador==2:
-	print("El año no es bisiesto")	
+if contador == 0 or contador == 2:
+	print("El año no es bisiesto")
 else:
-	print("El año es bisiesto")	
-
-
-
-
-
+	print("El año es bisiesto")
