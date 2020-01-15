09cb0e7a5a2ee95681273828668f951cb9d51d40
Nicolás A. Monzón <53285434+MonzonNicolas2019@users.noreply.github.com>
2020-01-15 11:54:02 -0300

    Enunciado
    Habia un error de interpretacion del enunciado pero nada mas, el resto son comentarios no tan importantes

--- #Ejercicio 6.py
+++ #Ejercicio 6.py
@@ -5,25 +5,26 @@ dias=int(input("Introduce la cantidad de dias desfasados:"))
 pago=0
 
 def funcion(tipo_de_video,dias):
+	global pago
 	if tipo_de_video=="Dibujo" and dias>0:
-		global pago
-		pago=1+0.5*(dias-1)
+		pago = 2
 
 	if tipo_de_video=="Estrenos" and dias>0:
-		pago=1+0.5*(dias-1)
-
+		pago = 3
 
 	if tipo_de_video=="Otros" and dias>0:
-		pago=1+0.5*(dias-1)
+		pago = 2.5
 
-	if dias<0:
-		pago=-1
+	if dias >= 1:
+		pago += 1 + 0.5*(dias-1)
 
-	if (tipo_de_video!="Otros" or tipo_de_video!="Estrenos" or tipo_de_video!="Dibujo") and dias>0:
-		print("El tipo de video introducido es incorrecto")		
+	#El siguiente If lo podes poner mas arriba para que programa se termine si llega a ser el caso y no haga nada, porque los if tambien tienen coste de ejecucion
+	if (tipo_de_video != "Otros" and tipo_de_video != "Estrenos" and tipo_de_video!="Dibujo") or dias < 0:
+		print("El tipo de video introducido es incorrecto o la cantidad de dias es incorrecta")
+		return -1 #No siempre, pero para algunos informaticos mas a la antigua, derivando de C++, ponen return -1 cuando el programa ejecuto algo incorrecto
 
-
-	print(pago)	
+	print("Pago: " + pago)
+	print("La cantidad de dias desfasados son " + (dias - 1))
 
 funcion(tipo_de_video,dias)	
 
